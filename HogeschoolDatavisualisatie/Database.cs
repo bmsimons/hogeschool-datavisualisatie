eef71b7f1918a9ac5e3644b394bdca5e5d4f2e8c
Bart Simons <bart@bartsimons.me>
2018-03-13 11:31:10 +0000

    Added generic class for database CRUD options

---
+++ HogeschoolDatavisualisatie/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Net.Sockets;
+
+namespace HogeschoolDatavisualisatie
+{
+    class Database
+    {
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<BsonDocument> collection;
+        private String address;
+        private String port;
+
+        public Database(string address = "127.0.0.1", string port = "27017", string database = "DataAggregation")
+        {
+            this.address = address;
+            this.port = port;
+            this.client = new MongoClient("mongodb://"+address+":"+port);
+            this.database = this.client.GetDatabase("DataAggregation");
+        }
+
+        private void SetCollection(string collection)
+        {
+            this.collection = this.database.GetCollection<BsonDocument>(collection);
+        }
+
+        private void WriteToDatabase(IEnumerable<BsonDocument> docs)
+        {
+            this.collection.InsertMany(docs);
+        }
+    }
+}
