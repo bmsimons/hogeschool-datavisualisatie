2a5a5f4f6f935775ad88748a80a5ffc708b6c866
Maikel Veen <veenmaikel@gmail.com>
2018-03-15 00:23:12 +0100

    Add string to nullable int extension method
    Extension class for string that makes it possible to convert a given string to a nullable int. Useful when parsing incomplete data text files.

---
+++ HogeschoolDatavisualisatie/Helpers/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Helpers
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Extension method that converts string to int but the int can also be null
+        /// </summary>
+        /// <param name="item">String to be converted</param>
+        /// <returns></returns>
+        public static int? TryGetInt32(this string item)
+        {
+            int i;
+            bool success = int.TryParse(item, out i);
+            return success ? (int?)i : (int?)null;
+        }
+    }
+}
