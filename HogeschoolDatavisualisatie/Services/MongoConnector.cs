372e3ae8d8e89b385af20c07b1a58b51910e0fd6
Maikel Veen <veenmaikel@gmail.com>
2018-03-20 00:23:29 +0100

    Add Mongo Database connector

---
+++ HogeschoolDatavisualisatie/Services/MongoConnector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+
+namespace HogeschoolDatavisualisatie.Services
+{
+    public class MongoConnector
+    {
+        private const string DatabaseName = "hogeschool";
+
+        private static MongoConnector instance = null;
+        private static readonly object padlock = new object();
+
+        private MongoClient mongoClient = null;
+        private IMongoDatabase mongoDatabase = null;
+        private string ipAddress = null;
+        private string port = null;
+
+        public MongoConnector(string ipAddress = "127.0.0.1", string port = "27017")
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+
+            this.mongoClient = new MongoClient("mongodb://" + ipAddress + ":" + port);
+            this.mongoDatabase = this.mongoClient.GetDatabase(DatabaseName);
+        }
+
+        public static MongoConnector Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new MongoConnector();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void InsertIntoDatabse(List<BsonDocument> documents, string collectionName)
+        {
+            IMongoCollection<BsonDocument> collection = this.mongoDatabase.GetCollection<BsonDocument>(collectionName);
+            collection.InsertMany(documents);
+        }
+
+        public void InsertIntoDatabse(BsonDocument document, string collectionName)
+        {
+            IMongoCollection<BsonDocument> collection = this.mongoDatabase.GetCollection<BsonDocument>(collectionName);
+            collection.InsertOne(document);
+        }
+    }
+}
