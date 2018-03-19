8f61a416d39a6c72924893a31eead0369c6fd0e6
Maikel Veen <veenmaikel@gmail.com>
2018-03-19 19:59:09 +0100

    Add MonthYear Class
    Month year class represents a date only by its month and year

---
+++ HogeschoolDatavisualisatie/DataModels/MonthYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace HogeschoolDatavisualisatie.DataModels
+{
+    public class MonthYear
+    {
+        private int year = 0;
+        private int month = 1;
+
+        public int Year { get => year; set => year = value; }
+        public int Month { get => month; set => month = value; }
+
+        public MonthYear(int year, int month)
+        {
+            this.year = year;
+            this.month = month;
+        }
+
+        public string GetMonthAsString()
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(this.month);
+        }
+        
+    }
+}
