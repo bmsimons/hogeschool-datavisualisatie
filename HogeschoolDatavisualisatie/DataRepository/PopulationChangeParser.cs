d6913e43326da9cd998247c6a0b71e876a9e31ad
Maikel Veen <veenmaikel@gmail.com>
2018-03-19 20:09:52 +0100

    Add PopulationChangeParser

---
+++ HogeschoolDatavisualisatie/DataRepository/PopulationChangeParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HogeschoolDatavisualisatie.DataModels;
+
+namespace HogeschoolDatavisualisatie.DataRepository
+{
+    class PopulationChangeParser : IDataParser<PopulationChangeModel, Tuple<int, int, MonthYear>, object>
+    {
+        public PopulationChangeModel ConvertDataToModel(Tuple<int, int, MonthYear> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, int, MonthYear> GetDataPoint(object i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParseData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
