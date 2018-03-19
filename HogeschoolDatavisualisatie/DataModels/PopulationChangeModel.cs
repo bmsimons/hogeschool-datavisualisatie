55772359947520b52bed4bd6bcdbc690f03473fe
Maikel Veen <veenmaikel@gmail.com>
2018-03-19 20:05:11 +0100

    Add Population change model

---
+++ HogeschoolDatavisualisatie/DataModels/PopulationChangeModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace HogeschoolDatavisualisatie.DataModels
+{
+    class PopulationChangeModel : DataModel
+    {
+        [JsonProperty("bornAlive")]
+        public int bornAlive = 0;
+
+        [JsonProperty("totalDeaths")]
+        public int totalDeaths = 0;
+
+        [JsonProperty("monthYear")]
+        public MonthYear monthYear;
+
+        public PopulationChangeModel(int bornAlive, int totalDeaths, MonthYear monthYear)
+        {
+            this.bornAlive = bornAlive;
+            this.totalDeaths = totalDeaths;
+            this.monthYear = monthYear;
+        }
+    }
+}
