2b6c22da3a8d90674901a6af1fd7dc859a53897b
Maikel Veen <veenmaikel@gmail.com>
2018-03-28 19:29:52 +0200

    Add Traffic to aggregator wrapper

--- HogeschoolDatavisualisatie/Core/Aggregator.cs
+++ HogeschoolDatavisualisatie/Core/Aggregator.cs
@@ -49,6 +49,54 @@ namespace HogeschoolDatavisualisatie.Core
             AddToDatabase<WeatherModelMonthly>(weatherModelMonthlies, "weather-month");
         }
 
+        public static void AggregateTrafficModel(string filePath)
+        {
+            TrafficParser trafficParser = new TrafficParser();
+            trafficParser.ParseData(filePath);
+
+            foreach (TrafficModel trafficResultItem in trafficParser.ListTraffic)
+            {
+                MongoConnector.Instance.InsertIntoDatabse(new BsonDocument {
+                                    {"datum",        trafficResultItem.Datum ?? ""},
+                                    {"jaar",         trafficResultItem.Jaar ?? ""},
+                                    {"mnd",          trafficResultItem.Mnd ?? ""},
+                                    {"dag",          trafficResultItem.Dag ?? ""},
+                                    {"ticvanri",     trafficResultItem.Ticvanri ?? ""},
+                                    {"ticvan",       trafficResultItem.Ticvan ?? ""},
+                                    {"richt",        trafficResultItem.Richt ?? ""},
+                                    {"hm",           trafficResultItem.Hm ?? ""},
+                                    {"oorz",         trafficResultItem.Oorz ?? ""},
+                                    {"begt",         trafficResultItem.Begt ?? ""},
+                                    {"stuur",        trafficResultItem.StUur ?? ""},
+                                    {"stmin",        trafficResultItem.StMin ?? ""},
+                                    {"eindt",        trafficResultItem.Eindt ?? ""},
+                                    {"einduur",      trafficResultItem.EindUur ?? ""},
+                                    {"eindmin",      trafficResultItem.EindMin ?? ""},
+                                    {"zwaarte",      trafficResultItem.Zwaarte ?? ""},
+                                    {"gemleng",      trafficResultItem.GemLeng ?? ""},
+                                    {"duur",         trafficResultItem.Duur ?? ""},
+                                    {"dagnr",        trafficResultItem.Dagnr ?? ""},
+                                    {"weeknr",       trafficResultItem.Weeknr ?? ""},
+                                    {"dagsoort",     trafficResultItem.Dagsoort ?? ""},
+                                    {"g_l",          trafficResultItem.G_L ?? ""},
+                                    {"provincie",    trafficResultItem.Provinci ?? ""},
+                                    {"routelet",     trafficResultItem.Routelet ?? ""},
+                                    {"routenum",     trafficResultItem.Routenum ?? ""},
+                                    {"routeoms",     trafficResultItem.Routeoms ?? ""},
+                                    {"naam_van",     trafficResultItem.Naam_Van ?? ""},
+                                    {"naam_naa",     trafficResultItem.Naam_Naa ?? ""},
+                                    {"hm_van",       trafficResultItem.Hm_Van ?? ""},
+                                    {"hm_naar",      trafficResultItem.Hm_Naar ?? ""},
+                                    {"traj_van",     trafficResultItem.Traj_Van ?? ""},
+                                    {"traj_naa",     trafficResultItem.Traj_Naa ?? ""},
+                                    {"flricht",      trafficResultItem.Flricht ?? ""},
+                                    {"filesagvwerk", trafficResultItem.FilesAgvWerk ?? ""},
+                                    {"idwerk",       trafficResultItem.IdWerk ?? ""}
+                                }, "rijkswaterstaat");
+
+            }
+        }
+
         public static void AddToDatabase<T>(List<T> modelList, string collection)
         {
             foreach (T model in modelList)
